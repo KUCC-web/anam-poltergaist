da39666ee3f0acd805566b5f69cfae04286b9db0
ffaass <rlatnghd95@gmail.com>
2018-02-18 21:03:23 +0900

    Store의 picture_url 필드 이름을 image_url 필드로 변경

--- quiz/models.py
+++ quiz/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Store(models.Model):
     name = models.CharField(max_length=100, null=False)
     score = models.IntegerField(default=0, null=False)
-    picture_url = models.CharField(max_length=1000, null=False)
+    image_url = models.CharField(max_length=1000, null=False)
