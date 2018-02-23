abb210c962b35b4e022c43722ca1efcd99bce82d
ffaass <rlatnghd95@gmail.com>
2018-02-23 16:13:45 +0900

    Quiz 뷰, 템플릿 추가

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from quiz import views
 
+app_name = 'quiz'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('quiz/', views.quiz, name='quiz'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from quiz.models import Store
 
 
-# Create your views here.
 def index(request):
     return render(request, 'quiz/index.html')
+
+
+def quiz(request):
+    step = request.session['step'] if 'step' in request.session else 1
+    if request.method == 'POST':
+        step += 1
+        request.session['step'] = step
+    store = Store.objects.first()
+    context = {
+        'step': step,
+        'store': store
+    }
+    return render(request, 'quiz/quiz.html', context)
