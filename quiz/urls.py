a90baaa7880aa461aa0c58c9a03a55ef25f2c6fd
ffaass <rlatnghd95@gmail.com>
2018-02-23 21:56:14 +0900

    퀴즈 form의 post요청을 처리하는 view를 분리하여 세션에 이전 선택 기록을 남기도록 변경
     - quiz_submit 뷰에서 form action을 처리하도록 분리하여 새로고침시에도 step이 올라가지 않도록 변경
     - 세션에 이전 선택 기록과 이전에 등장한 가게 id를 기록

--- quiz/urls.py
+++ quiz/urls.py
@@ -6,4 +6,5 @@ app_name = 'quiz'
 urlpatterns = [
     path('', views.index, name='index'),
     path('quiz/', views.quiz, name='quiz'),
+    path('submit/', views.quiz_submit, name='submit'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from quiz.models import Store
 
 
@@ -6,12 +6,28 @@ def index(request):
     return render(request, 'quiz/index.html')
 
 
+def quiz_submit(request):
+    if request.method == 'POST':
+        if 'prev_store' not in request.session:
+            return redirect('quiz:quiz')
+        step = request.session['step'] if 'step' in request.session else 1
+        request.session['step'] = step + 1
+
+        prev_store_list = request.session['prev_store_list'] if 'prev_store_list' in request.session else []
+        prev_store_list.append(request.session['prev_store'])
+
+        prev_pick_list = request.session['prev_pick_list'] if 'prev_pick_list' in request.session else []
+        prev_pick_list.append(True if 'Y' in request.POST else False)
+        request.session['prev_store_list'] = prev_store_list
+        request.session['prev_pick_list'] = prev_pick_list
+    return redirect('quiz:quiz')
+
+
 def quiz(request):
     step = request.session['step'] if 'step' in request.session else 1
-    if request.method == 'POST':
-        step += 1
-        request.session['step'] = step
+
     store = Store.objects.first()
+    request.session['prev_store'] = store.id
     context = {
         'step': step,
         'store': store
