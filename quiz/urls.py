713ce69eccc980f2f63600ee25cb7455d2884c90
ffaass <rlatnghd95@gmail.com>
2018-02-24 02:14:47 +0900

    결과 뷰 추가
     - 템플릿은 결과 리스트를 표시만 하는 상태
     - 템플릿에서 action url 바꾸던 것을 뷰에서 redirect하도록 변경

--- quiz/urls.py
+++ quiz/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('quiz/', views.quiz, name='quiz'),
     path('submit/', views.quiz_submit, name='submit'),
+    path('result/', views.result, name='result'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -10,26 +10,45 @@ def quiz_submit(request):
     if request.method == 'POST':
         if 'prev_store' not in request.session:
             return redirect('quiz:quiz')
-        step = request.session['step'] if 'step' in request.session else 1
+        step = request.session['step'] if 'step' in request.session else 0
         request.session['step'] = step + 1
 
         prev_store_list = request.session['prev_store_list'] if 'prev_store_list' in request.session else []
         prev_store_list.append(request.session['prev_store'])
 
-        prev_pick_list = request.session['prev_pick_list'] if 'prev_pick_list' in request.session else []
-        prev_pick_list.append(True if 'Y' in request.POST else False)
+        pick_list = request.session['pick_list'] if 'pick_list' in request.session else []
+        pick_list.append(True if 'Y' in request.POST else False)
         request.session['prev_store_list'] = prev_store_list
-        request.session['prev_pick_list'] = prev_pick_list
+        request.session['pick_list'] = pick_list
+
+        if request.session['step'] >= 10:
+            return redirect('quiz:result')
+
     return redirect('quiz:quiz')
 
 
 def quiz(request):
-    step = request.session['step'] if 'step' in request.session else 1
+    step = request.session['step'] if 'step' in request.session else 0
 
-    store = Store.objects.first()
+    store = Store.objects.get(pk=step+1)
     request.session['prev_store'] = store.id
     context = {
         'step': step,
         'store': store
     }
     return render(request, 'quiz/quiz.html', context)
+
+
+def result(request):
+    if 'step' not in request.session or request.session['step'] < 9:
+        return redirect('quiz:quiz')
+    store_list = Store.objects.filter(pk__in=request.session['prev_store_list'])
+    context = {
+        'pick_list': list(zip(store_list, request.session['pick_list']))
+    }
+    del request.session['step']
+    del request.session['prev_store']
+    del request.session['prev_store_list']
+    del request.session['pick_list']
+
+    return render(request, 'quiz/result.html', context)
