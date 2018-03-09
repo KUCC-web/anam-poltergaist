00cce500fe395c0d42ea87979613c8b648b0fa95
ffaass <rlatnghd95@gmail.com>
2018-03-09 22:21:08 +0900

    settings.py 수정
     - 프로덕션 환경을 위한 ALLOWED_HOSTS 추가
     - 파일에 로깅하기 위한 설정 추가

--- anam_poltergaist/settings.py
+++ anam_poltergaist/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'clubfair.kucc.co.kr'
+]
 
 
 # Application definition
@@ -121,31 +124,22 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-# database logging
-# LOGGING = {
-#     'disable_existing_loggers': False,
-#     'version': 1,
-#     'handlers': {
-#         'console': {
-#             # logging handler that outputs log messages to terminal
-#             'class': 'logging.StreamHandler',
-#             'level': 'DEBUG',  # message level to be written to console
-#         },
-#     },
-#     'loggers': {
-#         '': {
-#             # this sets root level logger to log debug and higher level
-#             # logs to console. All other loggers inherit settings from
-#             # root level logger.
-#             'handlers': ['console'],
-#             'level': 'DEBUG',
-#             'propagate': False, # this tells logger to send logging message
-#                                 # to its parent (will send if set to True)
-#         },
-#         'django.db': {
-#             'level': 'DEBUG'
-#         },
-#     },
-# }
+LOGGING = {
+    'disable_existing_loggers': False,
+    'version': 1,
+    'handlers': {
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'log/log')
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': False,
+        }
+    },
+}
