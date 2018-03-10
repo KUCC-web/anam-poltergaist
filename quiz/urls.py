360ece6fa6e1ec6b7d89b5defd40be45ea89d3bb
ffaass <rlatnghd95@gmail.com>
2018-03-11 05:19:35 +0900

    url 설정 변경

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,8 +4,7 @@ from quiz import views
 
 app_name = 'quiz'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('quiz/', views.quiz, name='quiz'),
-    path('submit/', views.quiz_submit, name='submit'),
+    path('<int:pk>/question/', views.question, name='question'),
+    path('submit/', views.question_submit, name='submit'),
     path('result/', views.result, name='result'),
 ]
