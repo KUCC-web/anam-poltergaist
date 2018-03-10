4e31932f14fba182641ae7491adc6aa844f58965
ffaass <rlatnghd95@gmail.com>
2018-03-10 20:34:45 +0900

    anam_poltergaist -> kucc_fair로 변환

--- kucc_fair/wsgi.py
+++ kucc_fair/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for anam_poltergaist project.
+WSGI config for kucc_fair project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "anam_poltergaist.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kucc_fair.settings")
 
 application = get_wsgi_application()
