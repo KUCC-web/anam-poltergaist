17e5358b9578957af9d82d99f2e9e49b71d9d93d
ffaass <rlatnghd95@gmail.com>
2018-03-11 02:11:19 +0900

    Quiz, Question 모델 수정
     - Quiz에 type 추가
     - Quiz에 이미지 url 추가
     - Quiz 표시 여부 필드 추가
     - Question의 answer 필드 추가
     - Question의 score필드의 default 값 변경

--- quiz/models.py
+++ quiz/models.py
@@ -2,19 +2,33 @@ from django.db import models
 
 
 class Quiz(models.Model):
+    TYPES = (
+        ('Q', 'Quiz'),
+        ('W', 'Worldcup')
+    )
+
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
+    content_type = models.CharField(max_length=1, default='Q', choices=TYPES)
+    image_url = models.CharField(max_length=1000, default='')
+    is_showing = models.BooleanField(default=False, null=False)
 
     def __str__(self):
         return self.name
 
 
 class Question(models.Model):
-    quiz = models.ForeignKey('Quiz', on_delete=models.CASCADE)
+    ANSWER = (
+        (True, 'Y'),
+        (False, 'N')
+    )
+
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     question = models.CharField(max_length=100, null=False)
-    score = models.IntegerField(default=0, null=False)
-    image_url = models.CharField(max_length=1000)
+    score = models.IntegerField(default=1, null=False)
+    image_url = models.CharField(max_length=1000, blank=True)
     description = models.CharField(max_length=1000, default="")
+    answer = models.BooleanField(default=True, choices=ANSWER)
 
     def __str__(self):
         return '{} ({})'.format(self.question, str(self.score))
@@ -36,7 +50,7 @@ class Question(models.Model):
 
 
 class Grade(models.Model):
-    quiz = models.ForeignKey('Quiz', on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     text = models.CharField(max_length=100)
     min = models.IntegerField(null=False)
     max = models.IntegerField(null=False)
