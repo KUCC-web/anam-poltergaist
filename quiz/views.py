d4b26fc15cddbad2b7cd2cdfe56ad4ee8a6856f6
ffaass <rlatnghd95@gmail.com>
2018-03-11 02:14:14 +0900

    모델 변경사항에 맞추어 뷰와 템플릿 수정
     - score 조절 알고리즘 삭제

--- quiz/views.py
+++ quiz/views.py
@@ -11,80 +11,59 @@ def index(request):
     return render(request, 'quiz/index.html')
 
 
-def quiz_submit(request):
+def question_submit(request):
     if request.method == 'POST':
-        if 'prev_quiz' not in request.session:
-            return redirect('quiz:quiz')
+        if 'prev_question' not in request.session:
+            return redirect('quiz:question')
 
-        pick_list = request.session['pick_list'] if 'pick_list' in request.session else []
-        if request.session['prev_quiz']['id'] in [pick['id'] for pick in pick_list]:
-            return redirect('quiz:quiz')
-        request.session['prev_quiz']['pick'] = True if 'Y' in request.POST else False
-        pick_list.append(request.session['prev_quiz'])
+        question = request.session['prev_question']
+        submit_list = request.session.get('submit_list', [])
+        if question['id'] in [pick['id'] for pick in submit_list]:
+            return redirect('quiz:question')
+        question['correct'] = True if question['answer'] in request.POST else False
+        submit_list.append(question)
 
-        step = request.session['step'] if 'step' in request.session else 0
-        request.session['step'] = step + 1
-        request.session['pick_list'] = pick_list
+        request.session['step'] = request.session.get('step', 0) + 1
+        request.session['submit_list'] = submit_list
 
         if request.session['step'] >= 10:
             return redirect('quiz:result')
 
-    return redirect('quiz:quiz')
+    return redirect('quiz:question')
 
 
-def quiz(request):
-    if 'step' not in request.session:
-        initialize_quiz(request)
-    step = request.session['step'] if 'step' in request.session else 0
-    pick_list = request.session['pick_list'] \
-        if 'pick_list' in request.session else []
+def question(request, quiz_id):
+    step = request.session.get('step', 0)
+    pick_list = request.session.get('submit_list', [])
 
-    prev_quiz_id_list = [item['id'] for item in pick_list]
-    if pick_list:
-        last_pick = pick_list[-1]
-        if last_pick['pick'] and last_pick['score'] < 5:
-            score = last_pick['score'] + 1
-        elif not last_pick['pick'] and last_pick['score'] > 1:
-            score = last_pick['score'] - 1
-        else:
-            score = last_pick['score']
-    else:
-        score = 1
+    prev_question_id_list = [item['id'] for item in pick_list]
+
     # 몇 개 안되서 다 가져와서 랜덤으로 뽑음
-    store_list = Question.objects.filter(~Q(pk__in=prev_quiz_id_list), score=score)
+    question_list = Question.objects.filter(~Q(pk__in=prev_question_id_list), quiz=quiz_id)
+    question = random.choice(question_list)
 
-    if not store_list:
-        store_list = Question.objects.filter(~Q(pk__in=prev_quiz_id_list), score=score - 1)
-
-    store = random.choice(store_list)
-
-    request.session['prev_quiz'] = model_to_dict(store)
+    request.session['prev_question'] = model_to_dict(question)
     context = {
         'step': step,
-        'store': store
+        'question': question
     }
     return render(request, 'quiz/quiz.html', context)
-
-
-def initialize_quiz(request):
-    request.session['step'] = 0
 
 
 def result(request):
     if 'step' not in request.session or request.session['step'] < 9:
         request.session.flush()
         return redirect('quiz:index')
-    pick_list = request.session['pick_list']
+    pick_list = request.session['submit_list']
     score = 0
-    for store in pick_list:
-        if store['pick']:
-            score += store['score']
+    for question in pick_list:
+        if question['correct']:
+            score += question['score']
 
     grade = Grade.objects.get(min__lte=score, max__gte=score)
     context = {
-        'pick_list': filter(lambda item: item['pick'] is False, pick_list),
-        'grade': grade.text,
-        'score': score
+        'incorrect_list': filter(lambda item: item['correct'] is False, pick_list),
+        'grade': grade.text
     }
     request.session.flush()
 
