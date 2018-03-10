444f3001aa5ed4543c3baf9cc4d3a0aefa60bd96
ffaass <rlatnghd95@gmail.com>
2018-03-11 05:20:17 +0900

    퀴즈/결과 뷰 수정

--- quiz/views.py
+++ quiz/views.py
@@ -1,10 +1,9 @@
-import bisect
 import random
 
 from django.db.models import Q
 from django.forms import model_to_dict
 from django.shortcuts import render, redirect
-from quiz.models import Question, Grade
+from quiz.models import Question, Grade, Quiz
 
 
 def index(request):
@@ -13,36 +12,48 @@ def index(request):
 
 def question_submit(request):
     if request.method == 'POST':
-        if 'prev_question' not in request.session:
-            return redirect('quiz:question')
+        if 'current_quiz' not in request.session:
+            return redirect('main:index')
 
-        question = request.session['prev_question']
-        submit_list = request.session.get('submit_list', [])
-        if question['id'] in [pick['id'] for pick in submit_list]:
-            return redirect('quiz:question')
-        question['correct'] = True if question['answer'] in request.POST else False
+        quiz_pk = request.session['current_quiz']
+        current_session = request.session.get(str(quiz_pk), {})
+        if not current_session:
+            return redirect('main:index')
+
+        question = current_session['prev_question']
+        submit_list = current_session.get('submit_list', [])
+        if question['id'] in [submit['id'] for submit in submit_list]:
+            return redirect('quiz:question', pk=quiz_pk)
+        question['correct'] = True if str(question['answer']) in request.POST else False
         submit_list.append(question)
 
-        request.session['step'] = request.session.get('step', 0) + 1
-        request.session['submit_list'] = submit_list
-
-        if request.session['step'] >= 10:
+        current_session['step'] = current_session.get('step', 0) + 1
+        current_session['submit_list'] = submit_list
+        request.session[str(quiz_pk)] = current_session
+        if current_session['step'] >= 10:
             return redirect('quiz:result')
 
-    return redirect('quiz:question')
+    return redirect('quiz:question', pk=quiz_pk)
 
 
-def question(request, quiz_id):
-    step = request.session.get('step', 0)
-    pick_list = request.session.get('submit_list', [])
+def question(request, pk):
+    current_session = request.session.get(str(pk), None)
+    if not current_session:
+        if not Quiz.objects.get(pk=pk):
+            return redirect('main:index')
+        else:
+            current_session = request.session[str(pk)] = {}
+    step = current_session.get('step', 0)
+    pick_list = current_session.get('submit_list', [])
+    request.session['current_quiz'] = pk
 
     prev_question_id_list = [item['id'] for item in pick_list]
 
     # 몇 개 안되서 다 가져와서 랜덤으로 뽑음
-    question_list = Question.objects.filter(~Q(pk__in=prev_question_id_list), quiz=quiz_id)
+    question_list = Question.objects.filter(~Q(pk__in=prev_question_id_list), quiz=pk)
     question = random.choice(question_list)
 
-    request.session['prev_question'] = model_to_dict(question)
+    current_session['prev_question'] = model_to_dict(question)
     context = {
         'step': step,
         'question': question
@@ -51,10 +62,15 @@ def question(request, quiz_id):
 
 
 def result(request):
-    if 'step' not in request.session or request.session['step'] < 9:
-        request.session.flush()
-        return redirect('quiz:index')
-    pick_list = request.session['submit_list']
+    quiz_pk = request.session['current_quiz']
+    current_session = request.session.get(str(quiz_pk), None)
+    if not current_session:
+        return redirect('main:index')
+
+    if 'step' not in current_session or current_session['step'] < 9:
+        del request.session[str(quiz_pk)]
+        return redirect('main:index')
+    pick_list = current_session['submit_list']
     score = 0
     for question in pick_list:
         if question['correct']:
@@ -65,6 +81,6 @@ def result(request):
         'incorrect_list': filter(lambda item: item['correct'] is False, pick_list),
         'grade': grade.text
     }
-    request.session.flush()
+    del request.session[str(quiz_pk)]
 
     return render(request, 'quiz/result.html', context)
